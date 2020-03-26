59c59670a7461a614bd7a0e3ff7996995042816c
kihyo9 <kihyo9@gmail.com>
2020-03-26 18:34:23 -0400

    rewriting previus messsage - actually for the grad desc algorithm

--- homework3/test.py
+++ homework3/test.py
@@ -3,6 +3,7 @@
 # finite difference with thomas method
 # determine optimum dx for each case
 ## maybe find the difference in solution with each successive decrease in dx?
+#test
 
 import numpy as np
 import matplotlib.pyplot as plt
